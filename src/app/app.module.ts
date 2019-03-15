554b0fd1bce6824a463a01ef4cab106ed7a8e219
Marc-Olivier <marc-olivier.gagnon.4@ulaval.ca>
2019-03-14 22:04:00 -0400

    feat: adding basic header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './modules/home/home.module';
+import { OldComponent } from './pages/old/old.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+
+// OLD STUF
 import { MatInputModule } from '@angular/material/input';
 import { ChartsModule } from 'ng2-charts';
-import { ChartComponent } from './chart/chart.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatTabsModule } from '@angular/material/tabs';
-import { TextInputComponent } from './text-input/text-input.component';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { TextBlocComponent } from './text-bloc/text-bloc.component';
-import { KeySizeComponent } from './key-size/key-size.component';
 import { MatCardModule } from '@angular/material/card';
-import { TabGroupComponent } from './tab-group/tab-group.component';
-import { SummaryComponent } from './summary/summary.component';
-import { TextEncryptionComponent } from './text-encryption/text-encryption.component';
-import { TextDecryptionComponent } from './text-decryption/text-decryption.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
-import { IndexCoincidenceComponent } from './index-coincidence/index-coincidence.component';
-import { KeyComponent } from './key/key.component';
-import { MonoalphabeticComponent } from './pages/monoalphabetic/monoalphabetic.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
-import { OldComponent } from './pages/old/old.component';
-import { HomeComponent } from './pages/home/home.component';
+import {ChartComponent} from './old/chart/chart.component';
+import {TextInputComponent} from './old/text-input/text-input.component';
+import {TextBlocComponent} from './old/text-bloc/text-bloc.component';
+import {KeySizeComponent} from './old/key-size/key-size.component';
+import {TabGroupComponent} from './old/tab-group/tab-group.component';
+import {SummaryComponent} from './old/summary/summary.component';
+import {TextEncryptionComponent} from './old/text-encryption/text-encryption.component';
+import {TextDecryptionComponent} from './old/text-decryption/text-decryption.component';
+import {IndexCoincidenceComponent} from './old/index-coincidence/index-coincidence.component';
+import {KeyComponent} from './old/key/key.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OldComponent,
+    NotFoundComponent,
+    // OLD Stuff
     ChartComponent,
     TextInputComponent,
     TextBlocComponent,
@@ -43,16 +48,15 @@ import { HomeComponent } from './pages/home/home.component';
     TextEncryptionComponent,
     TextDecryptionComponent,
     IndexCoincidenceComponent,
-    KeyComponent,
-    MonoalphabeticComponent,
-    NotFoundComponent,
-    OldComponent,
-    HomeComponent
+    KeyComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
+    CoreModule,
+    HomeModule,
+    // OLd STUF
     MatInputModule,
     ChartsModule,
     ReactiveFormsModule,
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatIconModule
+  ],
+  declarations: [
+    HeaderComponent,
+  ],
+  exports: [HeaderComponent]
+})
+export class CoreModule{ }
