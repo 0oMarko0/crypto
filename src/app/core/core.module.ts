bdbfba18b15713df88e1b153fabe6fe2d4a0b375
Marc-Olivier <marc-olivier.gagnon.4@ulaval.ca>
2019-03-19 15:31:06 -0400

    feat: header adding basic setting navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { SettingNavComponent } from './header/setting-nav/setting-nav.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SettingNavComponent],
   exports: [HeaderComponent]
 })
 export class CoreModule { }
